83ad92470da875cfd2950003e6f8b2d007723ca0
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2026-08-25 21:35:06 +0200

    VERY IMPORTANT label node. And refactored - created wave manager

---
+++ custom_nodes/wave_manager.gd
@@ -0,0 +1,39 @@
+class_name WaveManager
+extends Node
+
+signal enemy_spawn_requested(wave: Wave)
+
+@export var wave_array: Array[Wave]
+@export var wave_timer: Timer
+@export var spawn_interval_timer: Timer
+
+var current_wave_id := 0
+var current_wave: Wave
+
+
+func _ready() -> void:
+	wave_timer.timeout.connect(_on_wave_timer_timeout)
+	spawn_interval_timer.timeout.connect(_on_interval_timer_timeout)
+	if !wave_array.is_empty():
+		current_wave = wave_array[0]
+		current_wave.spawning_finished.connect(spawn_interval_timer.stop)
+		spawn_interval_timer.start(current_wave.get_interval_between_spawns())
+		wave_timer.start(current_wave.wave_length)
+
+
+func is_last_wave() -> bool:
+	return current_wave_id == wave_array.size()
+
+
+func _on_wave_timer_timeout() -> void:
+	current_wave_id += 1
+	if current_wave_id < wave_array.size():
+		current_wave = wave_array[current_wave_id]
+		current_wave.spawning_finished.connect(spawn_interval_timer.stop)
+		spawn_interval_timer.start(current_wave.get_interval_between_spawns())
+		wave_timer.start(current_wave.wave_length)
+
+
+func _on_interval_timer_timeout() -> void:
+	enemy_spawn_requested.emit(current_wave)
+	current_wave.register_enemy_spawn()
