e9372d3079608ed47a831e17d99a089334b858ee
feargrinn <tobyrogers000@gmail.com>
2026-08-30 10:12:34 +0200

    credits added

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -6,5 +6,9 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/test_scene.tscn")
 
 
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+
+
 func _on_quit_pressed() -> void:
 	get_tree().quit()
